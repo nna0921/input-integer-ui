2009791d6617a7866366bc304a3c3cd78a2bfb50
mac <mac@macs-MacBook-Air-2.local>
2025-10-24 23:39:24 +0500

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,17 @@
 const inputInteger = require('..')
-const x= inputInteger()
-document.body.append(x)
+const input1= inputInteger()
+const input2= inputInteger()
+const title= 'My demo form'
+const subtitle= 'Please fill out the form'
+const page = document.createElement('div')
+page.innerHTML= `
+<h1> ${title} </h1>
+<h2> ${subtitle} </h2>
+<h3>Enter your age</h3>
+<x></x>
+<h3>Enter your year of birth</h3>
+<y></y>
+`
+page.querySelector('x').replaceWith(input1)
+page.querySelector('y').replaceWith(input2)
+document.body.append(page)
