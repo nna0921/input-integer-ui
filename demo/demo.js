c12fe2d58a94007ab01b797522da908d138c330e
mac <mac@macs-MacBook-Air-2.local>
2025-10-24 22:22:44 +0500

    Added bundle.js

---
+++ demo/demo.js
@@ -0,0 +1,3 @@
+const inputInteger = require('..')
+const x= inputInteger()
+document.body.innerHTML= `<h1> ${x} </h1>`
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = inputInteger
+function inputInteger ()
+{
+    const name ='foo bar'
+    return name
+}
